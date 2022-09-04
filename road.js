ec104c70570d643917b0b196c12b78402fb4d3a4
root <root@DESKTOP-SJMT7AE.localdomain>
2022-09-03 20:01:52 -0500

    Updates and additions

--- road.js
+++ road.js
@@ -4,14 +4,6 @@ class Road{
         this.width = width;
         this.laneCount=laneCount;
 
-        for(let i=0;i<=this.laneCount; i++){
-            const x=lerp(
-                this.left,
-                this.right,
-                i/this.laneCount
-            )
-        }
-
         this.left=x-width/2;
         this.right=x+width/2;
 
@@ -21,18 +13,33 @@ class Road{
 
     }
 
+
     draw(ctx){
         ctx.lineWidth=5;
         ctx.strokeStyle="white";
 
-        ctx.beginPath();
-        ctx.moveTo(this.left, this.top);
-        ctx.lineTo(this.left, this.bottom);
-        ctx.stroke();
+        for(let i=0;i<=this.laneCount;i++){
+            const x=lerp(
+                this.left,
+                this.right,
+                i/this.laneCount
+            );
+            if(i>0 && i<this.laneCount){
+                ctx.setLineDash([20,20]);
+            }else{
+                ctx.setLineDash([]);
+            }
+            ctx.beginPath();
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x, this.bottom);
+            ctx.stroke();
 
+          
+        }
     }
-}
-
-function lerp(A,B,t){
-    return A+(B-A)*t;
+    getLaneCenter(laneIndex){
+        const laneWidth=this.width/this.laneCount;
+        return this.left+laneWidth/2+laneIndex*laneWidth;
+    }
+    
 }
