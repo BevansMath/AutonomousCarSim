6da827756fdf2b036cb53cef049bfb21050286d8
root <root@DESKTOP-SJMT7AE.localdomain>
2022-09-03 20:43:45 -0500

    Added moving road functionality

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ canvas.width=200;
 
 const ctx = canvas.getContext("2d");
 const road=new Road(canvas.width/2, canvas.width*0.9);
-const car=new Car(road.getLaneCenter(3),100,30,50);
+const car=new Car(road.getLaneCenter(1),100,30,50);
 
 car.draw(ctx);
 
@@ -14,8 +14,14 @@ function animate(){
     car.update();
 
     canvas.height=window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0, -car.y);
+
     road.draw(ctx);
     car.draw(ctx);
+
+    ctx.restore();
     requestAnimationFrame(animate); //calls the animate frame many times per second
 
     //if(car.update(this.controls.forward)=canvas.width){ //Constructing uncrossable borders
